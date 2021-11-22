fe1ce6a82016ba8f3942aa77eb65f5d87e37850c
noahward <noahward@shaw.ca>
2021-11-22 12:39:15 -0800

    Worked on user post functionality

--- src/client/php/createPost.php
+++ src/client/php/createPost.php
@@ -1,6 +1,11 @@
+
+/*TODO: The username of person making the post needs to be configured */
+/*TODO: Must know post id so handleImg.php knows what tuple to insert the post pic into */
+
 <?php
 
 include 'connectDB.php';
+include 'handleImg';
 
 $connection = connectToDB();
 
@@ -11,6 +16,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $postCat = $_POST['post_category'];
   $postImg = $_FILES['file']['name'];
 
+  // Insert post information into db
+  $sql = 'INSERT INTO Post (post_body, uname, cat_title, post_date, post_time) VALUES (?, ?, ?, CURDATE(), CURTIME());';
+  $stmt = $connection->prepare($sql);
+  $stmt->bind_param('sss', $postBody, $uname, $postCat);
+  $stmt->execute();
+
+  // Insert post img to db
+  uploadImgToDB($connection, $postImg, $uname, true);
 
 }
 // Handle GET requests
@@ -21,5 +34,7 @@ else if ($_SERVER["REQUEST_METHOD"] == "GET") {
           <a href="javascript:history.back()">Return to login screen</a>
         </div>';
 }
+
+mysqli_close($connection);
 
 ?>
--- src/client/php/handleImg.php
+++ src/client/php/handleImg.php
@@ -1,7 +1,7 @@
 <?php
 
 // Handle account creation image processing
-function uploadImgToDB ($connection, $fileName, $uname) {
+function uploadImgToDB ($connection, $fileName, $uname, $isPost = false) {
 
   $targetDir = '../uploads/';
   $targetFilePath = $targetDir . $fileName;
@@ -9,22 +9,49 @@ function uploadImgToDB ($connection, $fileName, $uname) {
   $allowTypes = array('jpg','png','jpeg','pdf');
   $file = $_FILES['file']['tmp_name'];
 
-  if(in_array($fileType, $allowTypes)){
-    // Upload file to server
-    if(move_uploaded_file($file, $targetFilePath)){
-      // Insert image file name into database
-      $insert = $connection->query("UPDATE Account SET pfp = '".$fileName."' WHERE uname = '".$uname."'");
-      
-      if($insert){
-        echo "The file ".$fileName. " has been uploaded successfully.";
-      }else{
-        echo "File upload failed, please try again.";
-      } 
-    }else{
-      echo "Sorry, there was an error uploading your file.";
+  // IF image is being uploaded in a post
+  if ($isPost) {
+    if (in_array($fileType, $allowTypes)) {
+      if (move_uploaded_file($file, $targetFilePath)) {
+
+        $insert = $connection->query("UPDATE Post SET post_pic = '".$fileName."' WHERE uname = '".$uname."'");
+        
+        if ($insert) {
+          echo "The file ".$fileName." has been uploaded successfully.";
+        }
+        else {
+          echo "File upload failed, please try again.";
+        } 
+      }
+      else {
+        echo "Sorry, there was an error uploading your file.";
+      }
     }
-  }else{
-    echo 'Sorry, only JPG, JPEG, PNG, & PDF files are allowed to upload.';
+    else {
+      echo 'Sorry, only JPG, JPEG, PNG, & PDF files are allowed to upload.';
+    }
+  }
+  // If image is being uploaded on account creation
+  else {  
+    if (in_array($fileType, $allowTypes)) {
+      if (move_uploaded_file($file, $targetFilePath)) {
+
+        $insert = $connection->query("UPDATE Account SET pfp = '".$fileName."' WHERE uname = '".$uname."'");
+        
+        if ($insert) {
+          echo "The file ".$fileName." has been uploaded successfully.";
+        }
+        else {
+          echo "File upload failed, please try again.";
+        } 
+      }
+      else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+    }
+    else {
+      echo 'Sorry, only JPG, JPEG, PNG, & PDF files are allowed to upload.';
+    }
   }
 }
 
