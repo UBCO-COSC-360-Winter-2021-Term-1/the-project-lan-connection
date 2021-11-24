ba15305c482d87a29bde93ff005191bc02cee56f
Andy McDonald <andymcd2000@gmail.com>
2021-11-23 19:58:21 -0800

    Update login.php
    implemented session on login page, also added security to accessing post values. Changed the way SQL request is done to put more computation on SQL rather than php

--- src/client/php/login.php
+++ src/client/php/login.php
@@ -3,49 +3,60 @@
 include 'connectDB.php';
 include 'validateText.php';
 
+// connect to DB
 $connection = connectToDB();
 
-// Handle POST requests
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// enable sessions
+session_start();
 
-  $email = validate($_POST['email']);
-  $pword = validate($_POST['password']);
-  $pword = md5($pword);
-
-  $sql = "SELECT uname, email, pword FROM Account;";
-  $results = mysqli_query($connection, $sql);
-
-  $match = false;
-  while ($row = mysqli_fetch_assoc($results))
-  {
-    // Check for matching username and password
-    if(($row['email'] == $email) && ($row['pword'] == $pword)) {
-      echo '<div style="display:flex; justify-content:center; align-items:center;"><p>The user <b>'.$row['uname'].'</b> has a valid account</p>';
-      $match = true;
-    }
-    else {
-      continue;
-    }
-  }
-  // Display a message to user on unsuccessful login attempt
-  if ($match == false) {
-    echo '<div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
-            <h1>Oh no!</h1>
-            <p>Username and/or password are invalid</p>
-            <a href="javascript:history.back()">Return to login screen</a>
-          </div>';
-  }
-
-}
-// Handle GET requests
-else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+// ensure using POST method
+if (!$_SERVER["REQUEST_METHOD"] == "POST") {
+  // not using POST
   echo '<div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
           <h1>Oh no!</h1>
           <p>Ensure request method is POST, not GET</p>
           <a href="javascript:history.back()">Return to login screen</a>
         </div>';
+  die();
 }
 
+// check if input fields set
+if (!isset($_POST['email']) || !isset($_POST['password'])) {
+  // not all inputs set
+  echo '<div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+          <h1>Oh no!</h1>
+          <p>Ensure both email and password are inputted</p>
+          <a href="javascript:history.back()">Return to login screen</a>
+        </div>';
+  die();
+}
+
+// get input values
+$email = validate($_POST['email']);
+$pword = validate($_POST['password']);
+$pword = md5($pword);
+
+// make SQL request
+$sql = "SELECT * FROM Account WHERE (email='$email' AND pword='$pword');";
+$results = mysqli_query($connection, $sql);
+
+// check if any results returned (ie. user exists in DB with that password)
+if ($row = mysqli_fetch_assoc($results)) {
+  // update session
+  $_SESSION['uname'] = $row['uname'];
+  // redirect user to home page
+  header("Location: ../html/home.php");
+}
+else {
+  // notify user of unsuccessful login attempt
+  echo '<div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+    <h1>Oh no!</h1>
+    <p>Username and/or password are invalid</p>
+    <a href="javascript:history.back()">Return to login screen</a>
+    </div>';
+}
+
+// close connection
 mysqli_close($connection);
 mysqli_free_result($results);
 
