0219f67eca6eafdc34b6727afec43cdf600bfda7
Andy McDonald <andymcd2000@gmail.com>
2021-12-02 10:46:38 -0800

    delete user works
    changed ddl foreign keys and updated delete user to match form of delete post

--- src/client/php/deleteUser.php
+++ src/client/php/deleteUser.php
@@ -10,4 +10,10 @@
 
   $sql = "DELETE FROM Account WHERE uname = '$uname'";
 
+  if (mysqli_query($connection, $sql)) {
+    return "<p>Successful user deletion</p>";
+  } else {
+    return "<p>Something went wrong, post was not deleted</p>";
+  }
+
 ?>
